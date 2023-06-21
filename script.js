fcf5b841c12a81630be4afe20395e24c95f5c6db
casanaa <antoniocasana@yahoo.com>
2023-06-21 17:55:49 -0500

    Found errors in previous code (generator finally works)

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-
+//Starting code
 var generateBtn = document.querySelector("#generate");
 var password = document.querySelector("#password");
 
@@ -13,18 +12,16 @@ var optionsVariable = "";
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
 function writePassword() {
-   var password = generatePassword();
-   var passwordText = document.querySelector("#password");
+    var password = generatePassword(); 
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
 
-   passwordText.value = password;
-
-
-
-
-    //Password criteria
-    var passwordLenght = prompt ("Choose a password length of at least 8 characters and no more than 128 characters.");
+// Write password to the #password input
+function generatePassword() {
+   //Password criteria
+    var passwordLength = prompt ("How many characters would you like your new password to contain? Choose between 8 and 128.");
     var num = confirm ("Include numbers in your password?");
     var uppercaseAlphabet = confirm ("Include uppercase letters in your password?");
     var lowercaseAlphabet = confirm ("Include lowercase letters in your password?");
@@ -33,12 +30,12 @@ function writePassword() {
     // Write another function that checks if it's between 8 and 128 characters
     if (passwordLength < 8 || passwordLength > 128) {
         alert("Your password is not between 8 characters and 128 characters. Please try again.");
-        var passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
+        var passwordLength = prompt("How many characters would you like your new password to contain? Choose between 8 and 128.");
     }
 
     // Ensure that if someone clicks cancel for all password criteria that it asks them to choose at least one
     else if (lowercaseLett === false && uppercaseLett === false && numbers === false && specialCharaters === false) {
-        alert("You must chose at least one password criteria.");
+        alert("You must choose at least one password criteria.");
         var lowercaseLett = confirm("Do you want your password to contain lowercase letters?");
         var uppercaseLett = confirm("Do you want your password to contain uppercase letters?");
         var num = confirm("Do you want your password to contain numbers?");
@@ -62,10 +59,15 @@ function writePassword() {
        optionsVariable += specialCharacters;
   }
 
+  var randomString = "";
     // Generates a random password based on the criteria that have been selected
     for (var i = 0; i < passwordLength; i++) {
         // Continues to select a random character value from the string until it is the desired length
         randomString += optionsVariable.charAt(Math.floor(Math.random() * optionsVariable.length));
+        
     }
-    password.value = randomString;
-}
+    
+    return randomString;
+      
+    }
+   
